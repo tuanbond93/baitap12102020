b0cb2230a726eff6083247c2b62657ee141e7e62
Tuan <sontuandav@gmail.com>
2020-10-12 21:54:16 +0700

    thu lenh listen

---
+++ bai tap 1/maytinh.js
@@ -0,0 +1,6 @@
+var selectedElement = document.getElementById("luachon");
+selectedElement.addEventListener("Click",tinh);
+function tinh() {
+    var selectvalue = document.getElementById("luachon").value;
+    alert(selectvalue)
+}
